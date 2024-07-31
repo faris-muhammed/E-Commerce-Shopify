dcb92f8bd6e53560f6f416b473d5c3ed3844de8a
Faris <farismuhammed707@gmail.com>
2024-07-31 07:44:09 +0530

    Latest update

--- controller/user/payment.go
+++ controller/user/payment.go
@@ -26,7 +26,6 @@ func RazorPay(c *gin.Context) {
 	var detail string
 
 	orderId := c.Query("id")
-	// Logged := c.GetUint("userid")
 
 	if err := initializer.DB.Preload("User").First(&payment, "payment_id=?", orderId).Error; err != nil {
 		c.JSON(404, gin.H{
@@ -38,15 +37,7 @@ func RazorPay(c *gin.Context) {
 		})
 		return
 	}
-	// if payment.UserId != Logged {
-	// 	c.JSON(404, gin.H{
-	// 		"Status":  "Error!",
-	// 		"Code":    404,
-	// 		"Message": "Order not found!",
-	// 		"Data":    gin.H{},
-	// 	})
-	// 	return
-	// }
+
 	if err := initializer.DB.Preload("Product").Find(&orderitems, "order_id=?", payment.OrderId).Error; err != nil {
 		c.JSON(400, gin.H{
 			"Status":  "Error!",
@@ -100,122 +91,122 @@ type Razor struct {
 }
 
 func RazorPayVerify(c *gin.Context) {
-    fmt.Println("")
-    fmt.Println("-----------------------------PAYMENT VERIFY------------------------")
+	fmt.Println("")
+	fmt.Println("-----------------------------PAYMENT VERIFY------------------------")
 
-    var verify Razor
-    var order []model.OrderItems
-    var payment model.PaymentDetails
-    var productQuantity model.ProductDetails
+	var verify Razor
+	var order []model.OrderItems
+	var payment model.PaymentDetails
+	var productQuantity model.ProductDetails
 
-    err := c.ShouldBindJSON(&verify)
-    if err != nil {
-        c.JSON(404, gin.H{
-            "Status":  "Error!",
-            "Code":    404,
-            "Error":   err.Error(),
-            "Message": "Couldn't bind JSON data!",
-            "Data":    gin.H{},
-        })
-        fmt.Println("Couldn't bind JSON data!")
-        fmt.Println("Received JSON:", verify)
-        return
-    }
+	err := c.ShouldBindJSON(&verify)
+	if err != nil {
+		c.JSON(404, gin.H{
+			"Status":  "Error!",
+			"Code":    404,
+			"Error":   err.Error(),
+			"Message": "Couldn't bind JSON data!",
+			"Data":    gin.H{},
+		})
+		fmt.Println("Couldn't bind JSON data!")
+		fmt.Println("Received JSON:", verify)
+		return
+	}
 
-    er := initializer.DB.First(&payment, "payment_id=?", verify.Order).Error
-    if er != nil {
-        c.JSON(404, gin.H{
-            "Status":  "Error!",
-            "Code":    404,
-            "Error":   er.Error(),
-            "Message": "No such order found!",
-            "Data":    gin.H{},
-        })
-        fmt.Println("No such order found!")
-        return
-    }
+	er := initializer.DB.First(&payment, "payment_id=?", verify.Order).Error
+	if er != nil {
+		c.JSON(404, gin.H{
+			"Status":  "Error!",
+			"Code":    404,
+			"Error":   er.Error(),
+			"Message": "No such order found!",
+			"Data":    gin.H{},
+		})
+		fmt.Println("No such order found!")
+		return
+	}
 
-    if err := initializer.DB.Preload("Order").Preload("Product").Find(&order, "order_id=?", payment.OrderId).Error; err != nil {
-        c.JSON(400, gin.H{
-            "Status":  "Error!",
-            "Code":    400,
-            "Error":   err.Error(),
-            "Message": "Couldn't find order items from database!",
-            "Data":    gin.H{},
-        })
-        fmt.Println("Couldn't find order items from database!")
-        return
-    }
+	if err := initializer.DB.Preload("Order").Preload("Product").Find(&order, "order_id=?", payment.OrderId).Error; err != nil {
+		c.JSON(400, gin.H{
+			"Status":  "Error!",
+			"Code":    400,
+			"Error":   err.Error(),
+			"Message": "Couldn't find order items from database!",
+			"Data":    gin.H{},
+		})
+		fmt.Println("Couldn't find order items from database!")
+		return
+	}
 
-    if verify.Status == "failed" {
-        payment.PaymentStatus = "failed"
-        initializer.DB.Save(&payment)
+	if verify.Status == "failed" {
+		payment.PaymentStatus = "failed"
+		initializer.DB.Save(&payment)
 
-        for _, val := range order {
-            val.PaymentStatus = "failed"
-            initializer.DB.Save(&val)
-        }
+		for _, val := range order {
+			val.PaymentStatus = "failed"
+			initializer.DB.Save(&val)
+		}
 
-        c.JSON(402, gin.H{
-            "Status":  "Error!",
-            "Code":    402,
-            "Message": "Payment failed!",
-            "Data":    gin.H{},
-        })
-        fmt.Println("Payment failed!")  // Make sure this line is executed
-        return
-    }
+		c.JSON(402, gin.H{
+			"Status":  "Error!",
+			"Code":    402,
+			"Message": "Payment failed!",
+			"Data":    gin.H{},
+		})
+		fmt.Println("Payment failed!")  // Make sure this line is executed
+		return
+	}
 
-    eror := RazorPaymentVerification(verify.Signature, verify.Order, verify.Payment)
-    if eror != nil {
-        c.JSON(402, gin.H{
-            "Status":  "Error!",
-            "Code":    402,
-            "Error":   eror.Error(),
-            "Message": "Payment verification failed!",
-            "Data":    gin.H{},
-        })
-        fmt.Println("Payment verification failed!")
-        return
-    }
+	eror := RazorPaymentVerification(verify.Signature, verify.Order, verify.Payment)
+	if eror != nil {
+		c.JSON(402, gin.H{
+			"Status":  "Error!",
+			"Code":    402,
+			"Error":   eror.Error(),
+			"Message": "Payment verification failed!",
+			"Data":    gin.H{},
+		})
+		fmt.Println("Payment verification failed!")
+		return
+	}
 
-    payment.TransactionId = verify.Payment
-    payment.PaymentStatus = "success"
-    erorr := initializer.DB.Save(&payment).Error
-    if erorr != nil {
-        c.JSON(400, gin.H{
-            "Status":  "Error!",
-            "Code":    400,
-            "Error":   erorr.Error(),
-            "Message": "Couldn't update payment success in database!",
-            "Data":    gin.H{},
-        })
-        fmt.Println("Couldn't update payment success in database!")
-        return
-    }
+	payment.TransactionId = verify.Payment
+	payment.PaymentStatus = "success"
+	erorr := initializer.DB.Save(&payment).Error
+	if erorr != nil {
+		c.JSON(400, gin.H{
+			"Status":  "Error!",
+			"Code":    400,
+			"Error":   erorr.Error(),
+			"Message": "Couldn't update payment success in database!",
+			"Data":    gin.H{},
+		})
+		fmt.Println("Couldn't update payment success in database!")
+		return
+	}
 
-    for _, val := range order {
-        initializer.DB.First(&productQuantity, val.ProductId)
-        productQuantity.Quantity -= val.Quantity
-        if err := initializer.DB.Save(&productQuantity).Error; err != nil {
-            fmt.Println("Failed to save updated quantity of products in db:", err)
-        }
-    }
+	for _, val := range order {
+		initializer.DB.First(&productQuantity, val.ProductId)
+		productQuantity.Quantity -= val.Quantity
+		if err := initializer.DB.Save(&productQuantity).Error; err != nil {
+			fmt.Println("Failed to save updated quantity of products in db:", err)
+		}
+	}
 
-    for _, val := range order {
-        val.PaymentStatus = "success"
-        if err := initializer.DB.Save(&val).Error; err != nil {
-            fmt.Println("Failed to update payment status for item:", err)
-        }
-    }
+	for _, val := range order {
+		val.PaymentStatus = "success"
+		if err := initializer.DB.Save(&val).Error; err != nil {
+			fmt.Println("Failed to update payment status for item:", err)
+		}
+	}
 
-    c.JSON(200, gin.H{
-        "Status":  "Success!",
-        "Code":    200,
-        "Message": "Payment Successful!",
-        "Data":    gin.H{},
-    })
-    fmt.Println("Payment Successful!")
+	c.JSON(200, gin.H{
+		"Status":  "Success!",
+		"Code":    200,
+		"Message": "Payment Successful!",
+		"Data":    gin.H{},
+	})
+	fmt.Println("Payment Successful!")
 }
 
 
